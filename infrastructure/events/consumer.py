588d2b10183cb99a652d169862a1c696f7888c4d
Mattia Matteini <matteinimattia@gmail.com>
2024-12-11 12:46:22 +0100

    refactor: replace threading with multiprocessing

--- infrastructure/events/consumer.py
+++ infrastructure/events/consumer.py
@@ -1,6 +1,6 @@
 import json
-import threading
-from typing import List
+from multiprocessing import Process
+from typing import List, Dict
 
 from kafka3 import KafkaConsumer
 from typing_extensions import Callable
@@ -15,24 +15,23 @@ class Consumer:
         self.is_consuming: bool = False
         self._brokers: List[KafkaBroker] = get_brokers_from_env()
         self._handler: Callable[[dict], None] = handler
-        self._consumer: KafkaConsumer = KafkaConsumer(
-            *topics,
+        self._topics = topics
+        logger.info(f"Connected to Kafka brokers: {self._brokers}")
+
+    def _consume_messages(self) -> None:
+        consumer = KafkaConsumer(
+            *self._topics,
             value_deserializer=lambda v: json.loads(v.decode("utf-8")),
-            bootstrap_servers=list(
-                map(lambda broker: f"{broker.host}:{broker.port}", self._brokers)
-            ),
+            bootstrap_servers=[f"{broker.host}:{broker.port}" for broker in self._brokers],
             auto_offset_reset="latest",  # Start consuming from the end of the topic.
         )
-        logger.info(f"Connected to Kafka brokers: {self._brokers}")
+
+        for message in consumer:
+            self._handler(message)
 
     def start_consuming(self) -> None:
         if self.is_consuming:
             raise ValueError("Consumer is already consuming")
-
-        def consume():
-            for message in self._consumer:
-                self._handler(message)
-
-        threading.Thread(target=consume).start()
-
         self.is_consuming = True
+        consumer_process = Process(target=self._consume_messages)
+        consumer_process.start()
