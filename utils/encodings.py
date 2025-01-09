95efdedece8d4c802d9147457bab9b28bf2649a3
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2025-01-09 18:27:50 +0100

    chore(style): remove ugly and useless info logs in encodings.py

--- utils/encodings.py
+++ utils/encodings.py
@@ -1,14 +1,8 @@
 import base64
-from typing import Union
-
-from utils.logs import logger
 
 
 def encode(value: str) -> str:
-    logger.info("Encoding value")
-    logger.info(type(value))
     encoded_bytes = base64.b64encode(value.encode("utf-8"))
-    logger.info("ENCODED")
     return encoded_bytes.decode("utf-8")
 
 
