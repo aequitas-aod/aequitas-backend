40d4e24bddb0ce41bec2962b6c17c81448e678c6
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2025-01-16 12:38:11 +0100

    feat: support automatic generation of the init.cypher script

---
+++ resources/db/__main__.py
@@ -0,0 +1,21 @@
+import sys
+from . import PATH_INIT_CYPHER
+from .cypher import (
+    overwrite_actual_general_context,
+    PATH_GENERAL_CONTEXT_CYPHER,
+    PATH_PROJECT_P1,
+)
+
+
+if len(sys.argv) < 2 or sys.argv[1] != "regenerate" or sys.argv[2] != "init":
+    print(f"Usage: python -m resources.db regenerate init")
+    exit(1)
+
+overwrite_actual_general_context()
+
+with PATH_INIT_CYPHER.open("w") as f:
+    f.write(PATH_GENERAL_CONTEXT_CYPHER.read_text())
+    f.write("\n\n")
+    f.write(PATH_PROJECT_P1.read_text())
+
+print(f"Regenerated {PATH_INIT_CYPHER}")
---
+++ resources/db/cypher/__init__.py
@@ -0,0 +1,69 @@
+from pathlib import Path
+
+
+DIR = Path(__file__).parent
+
+PATH_GENERAL_CONTEXT_CYPHER = DIR / "general-context.cypher"
+PATH_PROJECT_P1 = DIR / "project-p1.cypher"
+
+
+def _generate_kv_pair(key, value, indent=2, chunk_size=75):
+    def _indent(s, level=1):
+        return " " * indent * level + s
+
+    def _wrap(s, delimiter="'"):
+        s = s.replace(delimiter, f"\\{delimiter}")
+        return f"{delimiter}{s}{delimiter}"
+
+    result = f"{_indent(f'`{key}`', level=1)}:"
+    for i in range(0, len(value), chunk_size):
+        if i > 0:
+            result += " +"
+        result += f"\n{_indent(_wrap(value[i:i + chunk_size]), level=2)}"
+    return result
+
+
+def generate_general_context(**kwargs):
+    result = "CREATE (pc: PublicContext {"
+    for i, (k, v) in enumerate(kwargs.items()):
+        if i > 0:
+            result += ","
+        result += "\n"
+        result += _generate_kv_pair(k, v)
+    result += "\n});"
+    return result
+
+
+def generate_actual_general_context():
+    from resources.db import PATH_DATASETS
+    from resources.db.datasets import dataset_path, DIR as DATASETS_DIR
+    from application.automation.scripts.on_dataset_created import get_stats, get_heads
+    from application.automation.parsing import read_csv, to_csv
+    from utils.encodings import encode as base64encode
+
+    data = dict()
+
+    for dataset_name in DATASETS_DIR.glob("*.csv"):
+        dataset_name = dataset_name.stem
+        key_name = dataset_name.capitalize() + "-1"
+        path = dataset_path(dataset_name)
+        csv = path.read_text()
+        data[f"dataset__{key_name}"] = csv
+        df = read_csv(path)
+        stats = get_stats(df)
+        heads = get_heads(df)
+        data[f"stats__{key_name}"] = to_csv(stats)
+        data[f"dataset_head__{key_name}"] = to_csv(heads)
+
+    data["datasets"] = PATH_DATASETS.read_text()
+
+    for k in sorted(data.keys()):
+        print("Add key", k, "to general context")
+
+    data = {k: base64encode(v) for k, v in data.items()}
+    return generate_general_context(**data)
+
+
+def overwrite_actual_general_context():
+    PATH_GENERAL_CONTEXT_CYPHER.write_text(generate_actual_general_context())
+    print(f"Regenerated {PATH_GENERAL_CONTEXT_CYPHER}")
