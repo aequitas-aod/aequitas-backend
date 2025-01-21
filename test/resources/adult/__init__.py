22f3ed2c51bf7f5476b5014f09861548de5bf8fe
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2025-01-21 18:21:19 +0100

    test: remove useless test resources

--- test/resources/adult/__init__.py
+++ test/resources/adult/__init__.py
@@ -10,13 +10,11 @@ PATH_FEATURES_JSON = DIR / "features.json"
 PATH_SUGGESTED_PROXIES_JSON = DIR / "suggested-proxies.json"
 PATH_CORRELATION_MATRIX_SVG = DIR / "correlation-matrix.svg"
 PATH_ACTUAL_DATASET_CSV = DIR / "actual-dataset.csv"
-PATH_ACTUAL_DATASET_ADULT_CSV = DIR / "adult.csv"
 PATH_METRICS_JSON = DIR / "metrics.json"
 PATH_DETECTED_JSON = DIR / "detected.json"
 PATH_PROXIES_JSON = DIR / "proxies.json"
 PATH_PREPROCESSING_JSON = DIR / "preprocessing.json"
 PATH_PREPROCESSING_LFR_CSV = DIR / "preprocessed_lfr_result.csv"
-PATH_PREPROCESSING_LFR_CSV_OLD = DIR / "preprocessed_lfr_old.csv"
 PATH_PREPROCESSING_CR_CSV = DIR / "preprocessed_cr.csv"
 
 
