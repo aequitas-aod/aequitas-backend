3819c1cb767d22ee1ab9390e3d3c91c5c33bcd01
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2025-01-21 19:02:43 +0100

    chore: disable DB logging

--- infrastructure/storage/__init__.py
+++ infrastructure/storage/__init__.py
@@ -3,8 +3,12 @@ from utils.logs import logger
 
 _query_logs_count = 0
 
+DB_LOGGING = False
+
 
 def db_log(message, *args, **kwargs):
+    if not DB_LOGGING:
+        return
     global _query_logs_count
     logger.debug("[Neo4J#%d] " + message, _query_logs_count, *args, **kwargs)
     _query_logs_count += 1
