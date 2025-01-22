b39054d81a4fe35ae89304d0303938eff1fb20b5
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2025-01-22 14:57:40 +0100

    feat(ci): automate generation of docker images

--- release.config.mjs
+++ release.config.mjs
@@ -1,5 +1,11 @@
 import config from 'semantic-release-preconfigured-conventional-commits' assert { type: "json" };
 
+const imageTag = process.env.IMAGE_TAG;
+const dockerBuildLatestImage = `docker build -t ${imageTag} .`
+const dockerTagVersionedImage = `docker tag ${imageTag}:latest ${imageTag}:` + "${nextRelease.version}"
+const dockerPushLatestImage = `docker push ${imageTag}:latest`
+const dockerPushVersionedImage = `docker push ${imageTag}:` + "${nextRelease.version}"
+
 config.preset = 'conventionalcommits';
 config.tagFormat = 'v${version}';
 config.plugins.push(
@@ -7,7 +13,16 @@ config.plugins.push(
   "@semantic-release/release-notes-generator",
   "@semantic-release/changelog",
   "@semantic-release/github",
-  "@semantic-release/git"
+  "@semantic-release/git",
+  [
+    '@semantic-release/exec',
+    {
+      "publishCmd": dockerBuildLatestImage + "\n"
+        + dockerTagVersionedImage + "\n"
+        + dockerPushLatestImage + "\n"
+        + dockerPushVersionedImage  + "\n",
+    }
+  ]
 );
 
 export default config;
