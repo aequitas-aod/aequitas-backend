279cfe07d6cf95b8db8ba622b7073fbc2e6ca962
Mattia Matteini <matteinimattia@gmail.com>
2025-01-25 10:51:21 +0100

    fix: setup db script

--- setup-db.sh
+++ setup-db.sh
@@ -11,7 +11,7 @@ done
 curl -X DELETE http://localhost:4005/questions
 sleep 2
 # Create sample project
-curl -X POST -H "Content-Type: application/json" http://localhost:4005/projects -d '{"id": {"code": "p-1"}, "name": "Project 1"}'
+curl -X POST -H "Content-Type: application/json" http://localhost:4005/projects -d '{"code": "p-1", "name": "Project 1"}'
 sleep 1
 # Load questions in the database
 curl -X POST -H "Content-Type: text/yaml" --data-binary "@resources/questions-fairness-graph.yml" http://localhost:4005/questions/load
