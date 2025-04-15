1a88014d9b118e40dba3f16df47031dada03d0cc
Mattia Matteini <matteinimattia@gmail.com>
2025-04-15 17:01:12 +0200

    fix: trigger release

--- resources/adult/__init__.py
+++ resources/adult/__init__.py
@@ -5,7 +5,6 @@ import json
 DIR = Path(__file__).parent
 
 
-PATH_STATS_CSV = DIR / "stats.csv"
 PATH_FEATURES_JSON = DIR / "features.json"
 PATH_SUGGESTED_PROXIES_JSON = DIR / "suggested-proxies.json"
 PATH_CORRELATION_MATRIX_SVG = DIR / "correlation-matrix.svg"
